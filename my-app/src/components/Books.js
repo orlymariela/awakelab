40d1dc75dbd851417329930462533dfb79e4730f
orlymariela <omss1336@gmail.com>
2023-06-12 00:13:15 -0300

    Filter

--- my-app/src/components/Books.js
+++ my-app/src/components/Books.js
@@ -1,62 +1,65 @@
-import { useMemo, useEffect, useState} from 'react';
-import { useTable} from 'react-table';
+import React, { useState, useEffect } from 'react';
+import { useTable } from 'react-table';
 
-function Books(){
-    const [data, setData] = useState([]);
-    const columns = [
-        {
-          Header: 'Title',
-          accessor: 'name' // Establece el acceso a la propiedad 
-        },
-        {
-          Header: 'Autor',
-          accessor: 'authors'
-        },
-        {
-          Header: 'Editorial',
-          accessor: 'publisher'
-        }
-      ];
+function Books() {
 
-    useEffect(() => {
-         fetch('https://anapioficeandfire.com/api/books') 
-          .then(response => response.json())
-          .then(data => setData(data))
-          console.log(data)
-    }, []);
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
-        columns,
-        data: data,
-      });
-return(
-    <div>
-        <table {...getTableProps()}>
-  <thead>
-    {headerGroups.map(headerGroup => (
-      <tr {...headerGroup.getHeaderGroupProps()}>
-        {headerGroup.headers.map(column => (
-          <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-        ))}
-      </tr>
-    ))}
-  </thead>
+  const [books, setBooks] = useState([]);
 
-  <tbody {...getTableBodyProps()}>
-    {rows.map((row) => {
-     prepareRow(row)
-     return (
-       <tr {...row.getRowProps()}>
-         {row.cells.map(cell => {
-           return (<td {...cell.getCellProps()}>{cell.render('Cell')}</td>)
-         })}
-       </tr>
-     )
-   })}
-  </tbody>
+  useEffect(() => {
+    fetch('https://anapioficeandfire.com/api/books')
+      .then(response => response.json())
+      .then(data => setBooks(data))
+  }, []);
 
-</table>
-    </div>
-)
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Title',
+        accessor: 'name'
+      },
+      {
+        Header: 'Author',
+        accessor: 'authors'
+      },
+      {
+        Header: 'Publisher',
+        accessor: 'publisher'
+      }
+    ],
+    []
+  )
+
+  const tableInstance = useTable({ columns, data : books })
+
+   return (
+     <div>
+       <table {...tableInstance.getTableProps()}>
+          <thead>
+            {tableInstance.headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+
+          <tbody {...tableInstance.getTableBodyProps()}>
+            {tableInstance.rows.map(row => {
+              tableInstance.prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (<td {...cell.getCellProps()}>{cell.render('Cell')}</td>)
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+
+       </table>
+     </div>
+   );
 }
 export default Books;
 
