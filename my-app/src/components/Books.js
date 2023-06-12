5903ecfbc7436badcf7bb1666ada2f5cd5d76d8e
orlymariela <omss1336@gmail.com>
2023-06-12 00:14:04 -0300

    Filter

--- my-app/src/components/Books.js
+++ my-app/src/components/Books.js
@@ -32,9 +32,20 @@ function Books() {
   const tableInstance = useTable({ columns, data : books })
 
    return (
-     <div>
+    <div class="py-4">
+        <div class="grid text-center">
+            <h3 class="text-1xl font-bold tracking-tight text-gray-900 sm:text-2xl py-6">
+                What are you going to read today?</h3>
+            <div class="flex justify-center  ">
+                <input class="py-2 px-2 border-2 border-[#0a2415] rounded-md" type='text' placeholder='Search a tittle or name'></input>
+           </div>
+            <div>
+                <button class="p-2 rounded-md bg-[#0a2415] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#85d099] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#d9f2dd] m-4 transform hover:scale-110">Search</button>
+            </div>    
+        </div>
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
        <table {...tableInstance.getTableProps()}>
-          <thead>
+          <thead class="py-4 text-white bg-[#144026]">
             {tableInstance.headerGroups.map(headerGroup => (
               <tr {...headerGroup.getHeaderGroupProps()}>
                 {headerGroup.headers.map(column => (
@@ -59,6 +70,9 @@ function Books() {
 
        </table>
      </div>
+
+    </div>
+     
    );
 }
 export default Books;
